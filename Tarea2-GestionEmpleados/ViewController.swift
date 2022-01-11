2e08772e81da5b9e90dcf57cf15490c0e2443081
Francisco Javier <francisco_javier_ojeda_tsapp1ma2021@cev.com>
2022-01-11 09:18:02 +0100

    Initial Commit

---
+++ Tarea2-GestionEmpleados/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarea2-GestionEmpleados
+//
+//  Created by APPS2M on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
