e82e5b5b8df8bb4d0cdb5e8b0589f0d5496cb3dd
Jekuru <FJOAndreu@hotmail.es>
2022-01-28 10:42:55 +0100

    finalizado apimanager del login falta comprobar y realizar los cambios en el view

--- Tarea2-GestionEmpleados/APIManager/APIManager.swift
+++ Tarea2-GestionEmpleados/APIManager/APIManager.swift
@@ -8,6 +8,12 @@
 import Foundation
 import Alamofire
 
+enum APIErrors: Error{
+    case custom(message: String)
+}
+
+typealias Handler = (Swift.Result<Any?, APIErrors>) -> Void
+
 class APIManager{
     static let shareInstance = APIManager()
     
@@ -36,6 +42,34 @@ class APIManager{
             case .failure(let error):
                 print(error.localizedDescription)
                 completionHandler(false, "Algo ha pasado...")
+            }
+        }
+    }
+    
+    func callingLoginAPI(login: LoginModel, completionHandler: @escaping Handler){
+        let headers: HTTPHeaders = [
+            .contentType("application/json"),
+        ]
+        
+        AF.request(login_url, method: .put, parameters: login, encoder: JSONParameterEncoder.default, headers: headers).response{ response in
+            debugPrint(response)
+            switch response.result{
+            case .success(let data):
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                    print(json)
+                    if response.response?.statusCode == 200 {
+                        completionHandler(.success(json))
+                    }else {
+                        completionHandler(.failure(.custom(message: "Por favor, compruebe los datos introducidos.")))
+                    }
+                }catch{
+                    print(error.localizedDescription)
+                    completionHandler(.failure(.custom(message: "Por favor, vuelva a intentarlo.")))
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                completionHandler(.failure(.custom(message: "Por favor, compruebe los datos introducidos.")))
             }
         }
     }
---
+++ Tarea2-GestionEmpleados/Model/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  Tarea2-GestionEmpleados
+//
+//  Created by APPS2M on 28/1/22.
+//
+
+import UIKit
+
+struct LoginModel: Encodable{
+    let email: String
+    let password: String
+}
